78f8439e53d44e11a8b8d870d2d59639e6308ebd
C0C41N <0ct41n@gmail.com>
2021-04-16 16:13:28 +0500

    logo

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -3,12 +3,14 @@ import './App.scss';
 import React from 'react';
 
 import Close from '@comp/close';
+import Logo from '@comp/logo';
 
 function App() {
 	return (
 		<div className='container'>
 			<div id='main'>
 				<Close></Close>
+				<Logo></Logo>
 			</div>
 		</div>
 	);
