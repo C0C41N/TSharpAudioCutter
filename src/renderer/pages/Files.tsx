8433ca8b84e8d58724cdda750e7a86bfe340f163
C0C41N <0ct41n@gmail.com>
2021-04-26 11:32:37 +0500

    File

--- src/renderer/pages/Files.tsx
+++ src/renderer/pages/Files.tsx
@@ -41,28 +41,74 @@ const BtnDone = styled(btn)`
 
 const Container = styled.div`
 	position: absolute;
-	width: 800px;
-	height: 260px;
-	left: 110px;
-	top: 212px;
-
-	height: 260px;
-	width: 800px;
-	overflow: hidden;
+	width: 810px;
+	height: 280px;
+	left: 100px;
+	top: 202px;
 `;
 
 const List = styled.div`
-	width: 780px;
-	background-color: #ffffff;
+	width: calc(780px + 17px);
+	height: 100%;
+	overflow: scroll;
+
+	&::-webkit-scrollbar {
+		display: none;
+	}
 `;
 
 const File = styled.div`
 	height: 44px;
 	width: 780px;
+	background: #ffffff;
+	border-radius: 10px;
 
-	&:not(:last-child) {
-		margin-bottom: 10px;
+	margin: 10px;
+
+	box-shadow: 0px 4px 31px rgba(0, 0, 0, 0.0196802),
+		0px 1.6711px 12.9511px rgba(0, 0, 0, 0.0282725),
+		0px 0.893452px 6.92426px rgba(0, 0, 0, 0.035),
+		0px 0.500862px 3.88168px rgba(0, 0, 0, 0.0417275),
+		0px 0.266004px 2.06153px rgba(0, 0, 0, 0.0503198),
+		0px 0.11069px 0.85785px rgba(0, 0, 0, 0.07);
+
+	&:last-child {
+		margin-bottom: 100px;
 	}
+`;
+
+const Fade = styled.div`
+	position: absolute;
+	width: 794px;
+	height: 36px;
+	left: 100px;
+	bottom: 110px;
+
+	background: linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, #ffffff 100%),
+		linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, #ffffff 100%),
+		linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, #ffffff 100%);
+`;
+
+const Scroll = styled.div`
+	position: absolute;
+	width: 5px;
+	height: 260px;
+	right: 0px;
+	top: 10px;
+
+	background: #cccccc;
+	border-radius: 10px;
+`;
+
+const Thumb = styled.div`
+	position: absolute;
+	width: 3px;
+	height: 50px;
+	right: 1px;
+	top: 1px;
+
+	background: #3f3d56;
+	border-radius: 10px;
 `;
 
 function Files() {
@@ -84,7 +130,11 @@ function Files() {
 					<File />
 					<File />
 				</List>
+				<Scroll>
+					<Thumb />
+				</Scroll>
 			</Container>
+			<Fade />
 		</Fragment>
 	);
 }
