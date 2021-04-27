6ed5ce6fd247ab9cdffc5164f18bec908414f290
C0C41N <0ct41n@gmail.com>
2021-04-27 16:34:20 +0500

    fix

--- src/main/func.ts
+++ src/main/func.ts
@@ -9,11 +9,9 @@ export const conWin = (e: BrowserWindow, dev: boolean) => {
 	ipcMain.on('exit', () => e.close());
 
 	if (dev) {
-		const shortcuts = [
-			{ k: 'CommandOrControl+R', f: e.reload },
-			{ k: 'CommandOrControl+Shift+J', f: e.webContents.openDevTools },
-		];
-
-		shortcuts.forEach(({ k, f }) => globalShortcut.register(k, () => f()));
+		globalShortcut.register('CommandOrControl+R', () => e.reload());
+		globalShortcut.register('CommandOrControl+Shift+J', () =>
+			e.webContents.toggleDevTools()
+		);
 	}
 };
