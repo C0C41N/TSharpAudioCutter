86178f25d35ce5a14b7d4b79a355e5baecc4dffb
C0C41N <0ct41n@gmail.com>
2021-05-03 11:07:11 +0500

    state lic

--- src/renderer/services/state.ts
+++ src/renderer/services/state.ts
@@ -4,3 +4,7 @@ import { svcStore } from './store';
 const { state } = svcStore;
 
 export const { val: $files, set: $setFiles, get: $$files } = state<TraFileList>();
+
+export const { val: $lic, set: $setLic, get: $$lic } = state<Lic>();
+
+enum Lic {null, demo, paid, dev}
