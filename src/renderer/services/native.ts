2959b5c5b46c54067339739d46bc307f04aa02a7
C0C41N <0ct41n@gmail.com>
2021-05-09 01:14:53 +0500

    openFolder

--- src/renderer/services/native.ts
+++ src/renderer/services/native.ts
@@ -1,6 +1,7 @@
 import type * as FS from 'fs';
 import type * as Path from 'path';
 import type * as Electron from 'electron';
+import type * as ChildProcess from 'child_process';
 
 export const valid = () => !!(window?.process as any)?.type;
 
@@ -10,7 +11,12 @@ export const fs: typeof FS = window.require('fs');
 export const path: typeof Path = window.require('path');
 export const electron: typeof Electron = window.require('electron');
 
+const child_process: typeof ChildProcess = window.require('child_process');
+
 export const exit = () => {
 	const { ipcRenderer } = electron;
 	ipcRenderer.send('exit');
 };
+
+export const openFolder = (path: string) =>
+	child_process.exec(`start "" "${path}"`);
