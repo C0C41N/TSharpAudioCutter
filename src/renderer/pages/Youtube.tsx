8a81b6888e0e05dea7315252ca42495f4fac5d2f
C0C41N <0ct41n@gmail.com>
2021-05-09 15:29:01 +0500

    err

--- src/renderer/pages/Youtube.tsx
+++ src/renderer/pages/Youtube.tsx
@@ -2,15 +2,31 @@ import getYtId from 'get-youtube-id';
 import React, { Fragment, useState } from 'react';
 
 import Back from '@comp/back';
+import { useStates } from '@services';
 import { getFilePath, ytdl } from '@services/ytdl';
 import { Btn, Heading, Input, Percent, Progress } from '@styles/pages/youtube';
+import { Level } from '@types';
 
 const url = 'https://www.youtube.com/watch?v=YmMtrNA7BOA';
 
 function Youtube() {
 	const { trunc } = Math;
+	const { Modal } = useStates();
+	const { set: setModal } = Modal({ reactive: false });
 
 	const [progress, setProgress] = useState(0);
+
+	const showErr = (subDesc = '') => {
+		setModal({
+			show: true,
+			loading: false,
+			level: Level.error,
+			desc: 'Invalid URL',
+			subDesc,
+		});
+
+		setProgress(0);
+	};
 
 	const start = async () => {
 		setProgress(0.01);
@@ -18,9 +34,11 @@ function Youtube() {
 		const YTDL = await ytdl;
 		const vid = getYtId(url);
 
-		if (!vid) return;
+		if (!vid) return showErr();
 
 		YTDL.download(vid);
+
+		YTDL.on('error', (e: Error) => showErr(e.message));
 
 		YTDL.on('progress', e => setProgress(e.progress.percentage));
 
