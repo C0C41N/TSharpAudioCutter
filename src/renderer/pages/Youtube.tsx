0afaad1e714b960d3aa3c78e48ff07e8a9583b16
C0C41N <0ct41n@gmail.com>
2021-05-09 14:04:59 +0500

    ytdl vid

--- src/renderer/pages/Youtube.tsx
+++ src/renderer/pages/Youtube.tsx
@@ -1,18 +1,27 @@
+import getYtId from 'get-youtube-id';
 import React, { Fragment, useState } from 'react';
 
 import Back from '@comp/back';
 import { ytdl } from '@services/ytdl';
 import { Btn, Heading, Input, Percent, Progress } from '@styles/pages/youtube';
 
+const url = 'https://www.youtube.com/watch?v=YmMtrNA7BOA';
+
 function Youtube() {
 	const [progress, setProgress] = useState(25);
 
 	const start = async () => {
 		const YTDL = await ytdl;
-		// YTDL.download('https://www.youtube.com/watch?v=YmMtrNA7BOA');
-		YTDL.download('YmMtrNA7BOA');
-		YTDL.on('progress', e => console.log(JSON.stringify(e, null, '\t')));
-		YTDL.on('finished', e => console.log('Finished'));
+
+		const vid = getYtId(url);
+
+		if (!vid) return;
+
+		// YTDL.download(vid);
+		// YTDL.on('progress', e => console.log(JSON.stringify(e, null, '\t')));
+		// YTDL.on('finished', (e, d) =>
+		// 	console.log('Finished', JSON.stringify(d, null, '\t'))
+		// );
 	};
 
 	const gProgress = (
