b6732abf86c2956c860dd41fa8d7cb4ad01d6f51
C0C41N <0ct41n@gmail.com>
2021-05-30 17:33:42 +0500

    new redFlash

--- src/renderer/styles/pages/registration.ts
+++ src/renderer/styles/pages/registration.ts
@@ -29,28 +29,41 @@ export const Illustration = styled(registerIllus)`
 `;
 
 const redFlash = keyframes`
-	0%   {
+  0% {
 		border-color: red;
 		border-width: 2px;
 		transform: translateX(0);
 	}
 
-	25%   {
-		transform: translateX(5px);
-	}
-
-	50%   {
-		transform: translateX(-5px);
-	}
-
-  100% {
+	100% {
 		border-width: 2px;
-		transform: translateX(0);
-	}
+    transform: translateX(0);
+  }
+
+  10%,
+  30%,
+  50%,
+  70% {
+    transform: translateX(-10px);
+  }
+
+  20%,
+  40%,
+  60% {
+    transform: translateX(10px);
+  }
+
+  80% {
+    transform: translateX(8px);
+  }
+
+  90% {
+    transform: translateX(-8px);
+  }
 `;
 
 export const animRedFlash = css`
-	animation: ${redFlash} 0.3s forwards ease-out;
+	animation: ${redFlash} 0.8s cubic-bezier(0.455, 0.03, 0.515, 0.955) both;
 `;
 
 export const Input = styled(input)<{ redFlash: boolean }>`
