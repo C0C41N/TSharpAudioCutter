9fae1ef38cdbc060b8ba7ff5293d287518c0a8e3
C0C41N <0ct41n@gmail.com>
2021-05-31 08:48:42 +0500

    btn pressed

--- src/renderer/styles/comp.ts
+++ src/renderer/styles/comp.ts
@@ -3,6 +3,19 @@ import styled, { css, keyframes } from 'styled-components';
 import { flexCenter } from '@styles/classes';
 import { mont_400_14, mont_600_14 } from '@styles/fonts';
 
+// internal helpers
+
+const btnShadow = css`
+	box-shadow: -5px 5px 10px rgba(217, 217, 217, 0.2),
+		5px -5px 10px rgba(217, 217, 217, 0.2),
+		-5px -5px 10px rgba(255, 255, 255, 0.9),
+		5px 5px 13px rgba(217, 217, 217, 0.9);
+`;
+
+const btnBorder = css`
+	border: 1px solid #d9d9d9;
+`;
+
 const redFlash = keyframes`
   0% {
 		border-color: red;
@@ -37,25 +50,28 @@ const redFlash = keyframes`
   }
 `;
 
-export const animRedFlash = css`
+const animRedFlash = css`
 	animation: ${redFlash} 0.8s cubic-bezier(0.455, 0.03, 0.515, 0.955) both;
 `;
 
-// TODO: pressed effect
+// components
+
 export const btn = styled.div`
 	${flexCenter}
+	${btnBorder}
 	background: #ffffff;
-	border: 1px solid #d9d9d9;
 	box-sizing: border-box;
 	border-radius: 5px;
 	cursor: pointer;
 
 	&:hover {
+		${btnShadow}
 		border: none;
-		box-shadow: -5px 5px 10px rgba(217, 217, 217, 0.2),
-			5px -5px 10px rgba(217, 217, 217, 0.2),
-			-5px -5px 10px rgba(255, 255, 255, 0.9),
-			5px 5px 13px rgba(217, 217, 217, 0.9);
+	}
+
+	&:active {
+		${btnShadow}
+		${btnBorder}
 	}
 `;
 
