e64467bc2804d4079c852ee78bbae55240b50dec
C0C41N <0ct41n@gmail.com>
2021-08-01 20:54:47 +0500

    v1.2

--- src/renderer/common/const.ts
+++ src/renderer/common/const.ts
@@ -1,3 +1,3 @@
-export const version = 1.1;
+export const version = 1.2;
 export const appInitURL = 'https://us-central1-tsharp-audio-cutter.cloudfunctions.net/appInit';
 export const registerLicURL = 'https://us-central1-tsharp-audio-cutter.cloudfunctions.net/registerLic';
